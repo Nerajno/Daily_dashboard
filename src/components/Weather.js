a874399d145f3bb2b9c17cee9489863285d5285d
nerajno <nerajno@gmail.com>
2021-01-11 22:14:52 -0500

    fixed main error

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 let apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-let city = "Atlanta, USA";
+let city = "Atlanta";
 
 function Weather() {
     const [weather, setWeather] = useState(null);
     useEffect(() => {
         let api = `${apiUrl}?q=${city}&appid=${apiKey}&units=imperial`;
+        console.log(api);
         fetch(api)
         .then(response => response.json())
         .then(data => {setWeather(data);})
@@ -14,7 +15,7 @@ function Weather() {
 
 
     return (
-        <h2>The current weather is {weather}  in  {city}.</h2>
+        <h2>The current weather is {weather && weather.main.temp}  in  {city}.</h2>
     );
 }
 
